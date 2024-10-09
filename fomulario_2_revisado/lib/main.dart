1ac58f39bd1bad9c015d01055c816d2412c43192
Sergi Casanova Enguix <sergicasanova6798@gmail.com>
2024-10-09 14:54:47 +0200

    Nuevo formulario actualizado

---
+++ fomulario_2_revisado/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/form_register.dart';
+
+//importamos el formulario desde widgets.dart
+/* 
+La función main() es el punto de entrada de tu 
+aplicación Flutter. Es donde comienza la ejecución 
+del programa.
+*/
+void main() {
+  //Esta linea lanza la aplicacion
+  runApp(const MyApp());
+}
+
+//Aquí se configura el tema y la estructura de la app.
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  //Esta anotación indica que estás sobrescribiendo el método build de la clase base.
+  @override
+  /*
+    En el método build(), se construye la interfaz de usuario, 
+    definiendo un MaterialApp que establece temas y configura 
+    una página principal (MyHomePage).
+  */
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      /* 
+      Estableces el widget Scaffold como la pantalla principal de tu aplicación. 
+      Scaffold proporciona una estructura básica de diseño para tu aplicación, 
+      incluyendo el AppBar, el cuerpo, y más.
+      */
+      home: Scaffold(
+        //barra de aplicacion
+        appBar: AppBar(
+            //ponemos el titulo en el centro y definimos lo que hay escrito
+            title: const Center(child: Text('Register'))),
+        //establecemos el orden de los children en columnas, es decir de manera horizontal
+        body: const Column(
+          children: <Widget>[
+            //agregamos el form creado, el widget.
+            FormRegister()
+          ],
+        ),
+      ),
+    );
+    // Clase que define la página principal
+  }
+}
---
+++ fomulario_2_revisado/lib/widgets/form_register.dart
@@ -0,0 +1,227 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/infraestructure/models/usuario.dart';
+
+class FormRegister extends StatefulWidget {
+  const FormRegister({super.key});
+
+  @override
+  State<FormRegister> createState() => _FormRegisterState();
+}
+
+class _FormRegisterState extends State<FormRegister> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _nombreController = TextEditingController();
+  final TextEditingController _apellidosController = TextEditingController();
+  final TextEditingController _edadController = TextEditingController();
+  final TextEditingController _correoController = TextEditingController();
+
+  String? _sexoSeleccionado;
+
+  final List<String> _aficiones = [
+    'Deportes',
+    'Lectura',
+    'Cocinar',
+    'Videojuegos',
+    'Viajar'
+  ];
+  final List<bool> _aficionesSeleccionadas = List.generate(5, (_) => false);
+
+  String? _validarSexo() {
+    if (_sexoSeleccionado == null) {
+      return 'Por favor, selecciona un sexo';
+    }
+    return null;
+  }
+
+  @override
+  void dispose() {
+    _nombreController.dispose();
+    _apellidosController.dispose();
+    _edadController.dispose();
+    _correoController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextFormField(
+              controller: _nombreController,
+              decoration: const InputDecoration(
+                labelText: 'Nombre',
+                prefixIcon: Icon(Icons.person),
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'El nombre es obligatorio';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              controller: _apellidosController,
+              decoration: const InputDecoration(
+                labelText: 'Apellidos',
+                prefixIcon: Icon(Icons.person),
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'El apellido es obligatorio';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              controller: _edadController,
+              decoration: const InputDecoration(
+                  labelText: 'Edad', prefixIcon: Icon(Icons.person)),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'La edad es obligatoria';
+                }
+                final edad = int.tryParse(value);
+                if (edad == null) {
+                  return 'Por favor, introduce un número válido';
+                }
+                if (edad < 18) {
+                  return 'Debes ser mayor de 18 años';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              controller: _correoController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                prefixIcon: Icon(Icons.email),
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'El email es obligatorio';
+                }
+                // Validar el formato del email si es necesario
+                return null;
+              },
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                Expanded(
+                  child: RadioListTile<String>(
+                    title: const Text('Masculino'),
+                    value: 'Masculino',
+                    groupValue: _sexoSeleccionado,
+                    onChanged: (String? value) {
+                      setState(() {
+                        _sexoSeleccionado = value;
+                      });
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: RadioListTile(
+                    title: const Text('Femenino'),
+                    value: 'Femenino',
+                    groupValue: _sexoSeleccionado,
+                    onChanged: (String? value) {
+                      setState(() {
+                        _sexoSeleccionado = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  "Aficiones:",
+                  style: TextStyle(fontSize: 18),
+                ),
+                ..._aficiones.asMap().entries.map<Widget>((dynamic entry) {
+                  final index = entry.key;
+                  final aficion = entry.value;
+                  return CheckboxListTile(
+                    title: Text(aficion),
+                    value: _aficionesSeleccionadas[index],
+                    onChanged: (bool? value) {
+                      setState(() {
+                        _aficionesSeleccionadas[index] = value ?? false;
+                      });
+                    },
+                  );
+                }),
+              ],
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  if (!_aficionesSeleccionadas.contains(true)) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                          content:
+                              Text("Debes seleccionar al menos una afición")),
+                    );
+                  } else if (_validarSexo() == null) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                          content: Text("Debes seleccionar el sexo")),
+                    );
+                  } else {
+                    final usuario = Usuario(
+                      nombre: _nombreController.text,
+                      apellidos: _apellidosController.text,
+                      edad: int.parse(_edadController.text),
+                      correo: _correoController.text,
+                      sexo: _sexoSeleccionado!,
+                      aficiones: _aficiones
+                          .asMap()
+                          .entries
+                          .where((entry) => _aficionesSeleccionadas[entry.key])
+                          .map((entry) => entry.value)
+                          .toList(),
+                    );
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Usuario registrado")),
+                    );
+
+                    print(usuario); // Mostrar el usuario en consola
+
+                    // Limpiar los campos después de enviar
+                    _nombreController.clear();
+                    _apellidosController.clear();
+                    _edadController.clear();
+                    _correoController.clear();
+                    _sexoSeleccionado = null;
+                    _aficionesSeleccionadas.fillRange(
+                        0, _aficionesSeleccionadas.length, false);
+
+                    // Forzar la actualización de la interfaz
+                    setState(() {});
+                  }
+                }
+              },
+              child: const Text('Enviar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
